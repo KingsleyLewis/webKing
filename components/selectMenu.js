f99ab4ff0006b57ffd6052adfe22deb7b9ba13a3
klewis <klewis@KINGSLEYL30816.webex.local>
2010-10-13 13:36:46 -0700

    returns select component

--- components/selectMenu.js
+++ components/selectMenu.js
@@ -8,10 +8,18 @@ webKing.classify(selectMenu, {
 	multiple:true,
 	domNode:"",
 	
-	initiate:function(/*string*/json, /*string*/name){
+	initiate:function(){
+		json = this.arguments[0];
+		name = this.arguments[1];
 		select = document.createElement('input');
 		select.type = "select";
-		console.log(json[name]);
+		for(i=0; i<=json[name].length -1; i++){
+			var opt = document.createElement('option');
+			var val = document.createTextNode(json[name][i]);
+			opt.appendChild(val);
+			select.appendChild(opt);
+		}
+		this.domNode = select;
 	},
 
 	getDomNode:function(){
